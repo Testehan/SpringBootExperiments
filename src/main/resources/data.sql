e188ca29819524c8e83505e5405e8d9eaa7a4971
DanTestehan <tdan89@yahoo.com>
2023-06-12 16:36:40 +0300

    h2 database configured with web console and dummy data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into student(id,date_of_birth,email,name) values (1000,'2000-03-20','dana@yahoo.com','ciuciuca :)')
+insert into student(id,date_of_birth,email,name) values (1001,'1998-03-20','laura@yahoo.com','ciuciuca :{')
+insert into student(id,date_of_birth,email,name) values (1002,'1996-03-20','maria@yahoo.com','ciuciuca :/')
+insert into student(id,date_of_birth,email,name) values (1003,'1994-03-20','bolo@yahoo.com','ciuciuca :0')
