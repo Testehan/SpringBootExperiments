064ba17114e2275c4d3a9b62a198e4682ee8bcba
DanTestehan <tdan89@yahoo.com>
2023-07-26 17:08:45 +0300

    added School configuration that fails at container startup if the injection @Value is not found in the properties file

---
+++ src/main/java/com/testehan/SpringBootExperiments/docs/lookup/SchoolConfiguration.java
@@ -0,0 +1,19 @@
+package com.testehan.SpringBootExperiments.docs.lookup;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+//@PropertySource("classpath:application.properties")
+// Commented line from above because it is not needed
+// "Spring Boot configures by default a PropertySourcesPlaceholderConfigurer bean
+// that will get properties from application.properties and application.yml files."
+public class SchoolConfiguration {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertyPlaceholderConfigurer()
+    {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+}
